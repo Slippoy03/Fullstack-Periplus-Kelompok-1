70d74ece7cf167bcf56bdf74410d7dfe0587d490
Slippoy03 <dm93559@gmail.com>
2024-10-18 08:21:31 +0700

    updeath

--- frontend/Periplus-Fullstack-Project/services/axiosInstance.js
+++ frontend/Periplus-Fullstack-Project/services/axiosInstance.js
@@ -1,25 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 
-const apiURL = 'http://localhost:5000/api';
+const apiURL = "https://periplus-backend.vercel.app/api";
 
 const axiosInstance = axios.create({
-	baseURL: apiURL,
-	headers: {
-		'Content-Type': 'application/json',
-	},
+  baseURL: apiURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 axiosInstance.interceptors.request.use(
-	(config) => {
-		const token = localStorage.getItem('token');
-		if (token) {
-			config.headers.Authorization = token;
-		}
-		return config;
-	},
-	(error) => {
-		return Promise.reject(error);
-	}
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default axiosInstance;
